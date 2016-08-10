b50c068a1ebe863a59336be0ccd17a5f6b7f657a
Mance <marc.mance@gmail.com>
2016-08-09 23:57:52 -0400

    cache bust

--- majr.js
+++ majr.js
@@ -3,5 +3,10 @@ var majr = angular.module('majr', ['ngResource', 'ngRoute', 'ngSanitize', 'fireb
 
     function ($rootScope) {
      	console.log("9/10/16");
+      for (var key in window.localStorage) {
+      if (key.indexOf('http://marcmance.com/mjr/css/main.less') === 0) {
+          delete window.localStorage[key];
+        }
+      }
 	}
 ]);
